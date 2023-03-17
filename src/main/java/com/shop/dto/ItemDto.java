1b5c53fda3ec322db96063842db6120d7c64f889
rowanpark <rowan.park.83@gmail.com>
2023-03-17 15:41:03 +0900

    230317 commit 3, 243p 상품 등록 페이지 작성까지 완료

--- src/main/java/com/shop/dto/ItemDto.java
+++ src/main/java/com/shop/dto/ItemDto.java
@@ -11,11 +11,17 @@ import java.time.LocalDateTime;
 public class ItemDto {
 
     private Long id;
+
     private String itemNm;
+
     private Integer price;
+
     private String itemDetail;
+
     private String sellStatCd;
+
     private LocalDateTime regTime;
+
     private LocalDateTime updateTime;
 
 }
